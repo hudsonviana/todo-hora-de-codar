918e2879b751129292b5da4bd1ca322f1512be90
hudsonviana <hudson1206@gmail.com>
2024-06-13 23:12:09 -0300

    Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+import Search from './components/Search'
 import './App.css'
+import Filter from './components/Filter'
 
-function App() {
-  const [count, setCount] = useState(0)
+const todoData = [
+  {
+    id: 1,
+    text: 'Criar funcionalidade x no sistema',
+    category: 'Trabalho',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    text: 'Ir para academia',
+    category: 'Pessoal',
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: 'Estudar React',
+    category: 'Estudos',
+    isCompleted: false,
+  },
+]
+
+const App = () => {
+  const [todos, setTodos] = useState(todoData)
+  const [search, setsearch] = useState('')
+  const [filter, setFilter] = useState('all')
+  const [sort, setSort] = useState('asc')
+
+  const addTodo = (text, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 1000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ]
+
+    setTodos(newTodos)
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    )
+    setTodos(filteredTodos)
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => {
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+    })
+
+    setTodos(newTodos)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+
+      <Search search={search} setSearch={setsearch} />
+
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+
+      <div className="todo-list">
+        {todos
+          .filter((todo) =>
+            filter === 'all'
+              ? true
+              : filter === 'completed'
+              ? todo.isCompleted
+              : !todo.isCompleted
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === 'asc'
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <TodoForm addTodo={addTodo} />
+    </div>
   )
 }
 
